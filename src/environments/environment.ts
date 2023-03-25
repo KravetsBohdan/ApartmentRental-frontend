b7bbead21e363c87d15ff2eacdb95a08a17ae099
KravetsBohdan <bodyakravets@gmail.com>
2023-03-25 14:50:55 +0200

    change api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  API: 'https://railway.app/project/2c7265ff-8491-4a86-bcd9-09852efcee5f/api'
+  API: 'https://apartmentrentalapplication-production.up.railway.app/api'
 };
