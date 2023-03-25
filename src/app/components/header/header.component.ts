681e9bf3c5b7506310bee269eaa0a6764ac23fbd
KravetsBohdan <bodyakravets@gmail.com>
2023-03-25 14:03:55 +0200

    change import

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnChanges, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../services";
-import {UserService} from "../../services/user.service";
+import {UserService} from "../../services";
 import {User} from "../../interfaces";
 
 @Component({
